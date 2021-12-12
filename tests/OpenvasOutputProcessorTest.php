bf2750a0d94d078990bb0fd45277acd82e557b63
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2021-12-12 11:01:12 +0000

    Openvas test summary parsing

--- tests/OpenvasOutputProcessorTest.php
+++ tests/OpenvasOutputProcessorTest.php
@@ -11,6 +11,7 @@ class OpenvasOutputProcessorTest extends ParserTestCase
         $vulnerabilities = $processor->parseVulnerabilities($this->getResourceFilePath('openvas-v7.xml'));
         $this->assertCount(6, $vulnerabilities);
         $this->assertEquals('The configuration of this services should be changed sothat it does not support the listed weak ciphers anymore.', $vulnerabilities[4]->remediation);
+        $this->assertEquals('This routine search for weak SSL ciphers offered by a service.', $vulnerabilities[4]->summary);
     }
 
     public function testParseVulnerabilitiesIncludingCvssData()
